964dac0423570b3ae561fdcceb18f0a70295d7c3
saravanan-22 <saravanan.skillsafari@gmail.com>
2023-12-12 20:05:39 +0530

    Klendy_intQues

---
+++ String_Methods/script.js
@@ -0,0 +1,14 @@
+// const message = "Hello world";
+
+// console.log(message.slice(0 , 3));
+// let firstWord = message.slice(0, 3);
+// let secondWord = message.slice(6, 11);
+// let output = `The output is : ${firstWord} ${secondWord}`;
+// console.log(output);
+// Hel world//
+const pattern = /lo/g
+const message = "hello world ";
+console.log(message.match(pattern))
+const modifiedMessage = message.replace(pattern, "");
+
+console.log(modifiedMessage);
